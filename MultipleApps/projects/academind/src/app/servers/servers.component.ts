144859094afe64cd356eae65fe2059a86ed702b7
Greg LaPlant <glaplantwd@gmail.com>
2021-04-30 10:50:37 -0500

    Tutorials

---
+++ MultipleApps/projects/academind/src/app/servers/servers.component.ts
@@ -0,0 +1,39 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-servers',
+  templateUrl: './servers.component.html',
+  styleUrls: ['./servers.component.css']
+})
+export class ServersComponent implements OnInit {
+
+  allowNewServer = false ;
+  serverCreationStatus = 'No server was created!';
+  serverName = "Test Server";
+  userName = "";
+
+  constructor() {
+    setTimeout(()=>{
+      this.allowNewServer = true;
+    },2000)
+   }
+
+  
+
+  ngOnInit(): void {
+}
+  onCreateServer(){
+    this.serverCreationStatus = 'Server was Created! Name is ' + this.serverName;
+  }
+
+  onUpdateServerName(event: Event){
+   this.serverName= (<HTMLInputElement>event.target).value;
+
+  }
+
+
+  clearInput(event: Event){
+    this.userName = (<HTMLInputElement>event.target).value;
+  }
+
+}
